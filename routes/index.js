02b7b47cd7a8e15b49670c6bb3b472b0cda49e66
dark <123189717@qq.com>
2017-06-20 20:31:23 +0800

    修改获取openID返回值 bug

--- routes/index.js
+++ routes/index.js
@@ -64,7 +64,7 @@ router.get('/getOpenId', async (ctx, next) => {
   await fetch(`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=${grant_type}`)
     .then(res => res.json())
     .then(json => {
-      ctx.body = ctx.body = response.buildRes(200, true, json)
+      ctx.body = response.buildRes(200, true, json)
     })
 })
 
