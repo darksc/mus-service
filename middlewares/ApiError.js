c4ffbae20a6bf31f628e189f21a09406b64e9539
dark <123189717@qq.com>
2017-06-26 20:56:10 +0800

    加入异常处理实现方法

--- middlewares/ApiError.js
+++ middlewares/ApiError.js
@@ -1,3 +1,10 @@
-/**
- * Created by darK on 2017/6/23.
- */
+const ErrorNames = require('./ErrorNames')
+module.exports = class ApiErrors extends Error{
+  constructor(error_name){
+    super()
+    let error_info = ErrorNames.getErrorInfo(error_name)
+    this.name = error_name
+    this.code = error_info.code
+    this.message = error_info.message
+  }
+}
