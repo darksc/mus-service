0a299714a1664333d5b620928ed27f2bd9498b5a
dark <123189717@qq.com>
2017-06-28 13:05:20 +0800

    添加代码注释

--- middlewares/ErrorNames.js
+++ middlewares/ErrorNames.js
@@ -1,13 +1,16 @@
 let ErrorNames = {}
 
+// names 常量
 ErrorNames.UNKNOW_ERROR = "unknowError"
 ErrorNames.USER_NOT_EXIST = "userNotExist"
 
 const error_map = new Map()
 
+// 异常状态对象
 error_map.set(ErrorNames.UNKNOW_ERROR, { code: -1, message: '未知错误' })
 error_map.set(ErrorNames.USER_NOT_EXIST, { code: 101, message: '用户不存在' })
 
+// 获取异常状态方法
 ErrorNames.getErrorInfo = (error_name) => {
   let error_info
   if (error_name) {
