b1b88a1b4ec4a01abfcd9a1b5fc1a88d294d21ee
dark <123189717@qq.com>
2017-07-04 18:30:05 +0800

    拆分controller

--- controller/index.js
+++ controller/index.js
@@ -19,3 +19,41 @@ module.exports = async function shopService (ctx, next) {
     ctx.body = shop
   })
 }
+
+module.exports = async function addShopService (ctx, next) {
+  await Shop
+    .build({
+      id: 'mus' + uuidV4(),
+      name: ctx.body['name'],
+      address: ctx.body['address'],
+      phone: ctx.body['phone'],
+      price: ctx.body['price'],
+      latitude: ctx.body['latitude'],
+      longitude: ctx.body['longitude'],
+      time: ctx.body['time'],
+      nexttime: ctx.body['nexttime'],
+      discount: ctx.body['discount'],
+      discountInfo: ctx.body['discountInfo'],
+      event: ctx.body['event'],
+      eventInfo: ctx.body['eventInfo'],
+      game: ctx.body['game'],
+      gameInfo: ctx.body['gameInfo']
+    })
+    .save()
+    .then(shop => {
+      ctx.body = ''
+    })
+}
+
+module.exports = async function getOpenId (ctx, next) {
+  const code = ctx.query['code'],
+    appid = 'wx2ac89fd89e184752',
+    secret = '5ff66fda941bbf4f23b0062cdcb11cbc',
+    grant_type = 'authorization_code'
+
+  await fetch(`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=${grant_type}`)
+    .then(res => res.json())
+    .then(json => {
+      ctx.body = json
+    })
+}
