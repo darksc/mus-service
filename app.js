1d7d89893a5914ade02a2368660dfc691f23b16d
dark <123189717@qq.com>
2017-07-06 19:50:35 +0800

    优化数据库连接

--- app.js
+++ app.js
@@ -49,13 +49,4 @@ app.use(response)
 // routes
 app.use(index.routes(), index.allowedMethods())
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
 module.exports = app
--- db/connect.js
+++ db/connect.js
@@ -13,4 +13,13 @@ const sequelize = new Sequelize('mus', 'root', '', {
   }
 })
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 module.exports = sequelize
